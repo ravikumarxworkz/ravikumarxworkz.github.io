e11a14d68101137658de4dbd5409331cf7483eb0
RAVIKUMAR SHANNKAR KUMBAR <147067056+ravikumarxworkz@users.noreply.github.com>
2024-02-05 03:50:16 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showPage(pageId) {
-    document.querySelectorAll('section').forEach(section => {
-        section.style.display = 'none';
-    });
-
-    document.getElementById(pageId).style.display = 'block';
-}
-
