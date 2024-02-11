91650da0d7de920b019f2503994f6e7454d6d500
RAVIKUMAR SHANNKAR KUMBAR <147067056+ravikumarxworkz@users.noreply.github.com>
2024-02-11 18:23:44 +0530

    script

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+// ===========================show page========================
 function showPage(pageId) {
     document.querySelectorAll('section').forEach(section => {
         section.style.display = 'none';
@@ -6,49 +9,80 @@ function showPage(pageId) {
     document.getElementById(pageId).style.display = 'block';
 }
 
+//=============================== navigation========================
+function toggleNavbar() {
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse.classList.contains('show')) {
+        navbarCollapse.classList.remove('show');
+    }
+}
+
 $(document).ready(function () {
-    // Add a scroll event listener
     $(window).scroll(function () {
-        // Check if the user has scrolled down
         if ($(this).scrollTop() > 100) {
-            // If yes, add the "fixed-top" class to the navbar
             $('.navbar').addClass('fixed-top');
         } else {
-            // If no, remove the "fixed-top" class
             $('.navbar').removeClass('fixed-top');
         }
     });
 });
 
-  // typing text animation script
-  var typed = new Typed(".typing", {
-    strings: ["Developer", "Designer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-var typed = new Typed(".typing-2", {
-    strings: ["Developer", "Designer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down past a certain point, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+function scrollFunction() {
+  
+    const scrollThreshold = 20;
+
+    if (document.body.scrollTop > scrollThreshold || document.documentElement.scrollTop > scrollThreshold) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 1;
+}
+
+   function displayWords() {
+       let currentIndex = 0;
+       const words = ['Devloper', 'Designer'];
+       function updateWord() {
+           document.getElementById('words').innerText = words[currentIndex];
+           currentIndex = (currentIndex + 1) % words.length;
+       }
+       updateWord();
+
+       setInterval(updateWord, 2000); 
+   }
+   displayWords();
+
+//message me
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw3C3x7wvRNLUx0ntDS28PmeExjqx7fJnQ7leA-NOVTIMW87j1NoAXYdZWYzG1GHaUUpw/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
 
 
-// function sendEmail() {
-//     var params = {
-//         name: document.getElementById("name").value,
-//         email: document.getElementById("email").value,
-//         message: document.getElementById("message").value
-//     };
-//     const serviceID = "service_l48aw6h";
-//     const templateID = "template_uly1sc5";
-//     emailjs.send(serviceID, templateID, params).then(res => {
-//         document.getElementById("name").value = "";
-//         document.getElementById("email").value = "";
-//         document.getElementById("message").value = "";
-//         console.log(res);
-//         alert("your message sent sucessfully")
-//     })
-//         .catch((err) => console.log(err));
-// }
+
+
+
+
+
+
+
+
+
